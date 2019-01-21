b347d134d8beae05903f979f0b2861f992dcca54
Amannor <alon.mannor@gmail.com>
2019-01-21 19:03:08 +0200

    Powershell script to deploy filebeat in Windows
    Filebeat zip file for Windows + Powershell script to extract & install it

---
+++ deployFilebeat/filebeatDeployer.ps1
@@ -0,0 +1,22 @@
+﻿$PWD = (Get-Item -Path ".\").FullName
+$fullSrcPath = Join-Path $PWD "filebeat-6.5.4-windows-x86_64.zip"
+$dest =  Join-Path $PWD "tmpDir"
+
+Write-Host("*****Extracting $fullSrcPath to $dest*****")
+Expand-Archive $fullSrcPath $dest
+Write-Host("*****Finished extracting*****")
+
+$filebeatExecutableName = "filebeat.exe"
+$filebeatContentDir = Get-ChildItem -Recurse -Filter $filebeatExecutableName | Select-Object -ExpandProperty DirectoryName -Unique
+# Join-Path $PWD $filebeatContentDir "*"
+$filebeatFinalLocation = "C:\tmp\tmp folder with spaces\existingDir\newDir"#"C:\Program Files\Filebeat"
+Write-Host("*****Found $filebeatExecutableName in $filebeatContentDir - copying to $filebeatFinalLocation*****")
+robocopy $filebeatContentDir $filebeatFinalLocation /MIR /W:5 /R:2 
+Write-Host("*****Finished copying, deleting $fullSrcPath*****")
+Write-Host($dest)
+Remove-Item –path "$dest" –recurse 
+
+
+# $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
+
+
