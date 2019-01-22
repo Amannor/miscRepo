e499d019fce1130e8ff5042b3077f642698a4295
Amannor <alon.mannor@gmail.com>
2019-01-22 18:30:22 +0200

    Added code that updates the filebeat config file
    Updates it according to Coralogix params

--- deployFilebeat/filebeatDeployer.ps1
+++ deployFilebeat/filebeatDeployer.ps1
@@ -1,54 +1,57 @@
-$serviceName = 'filebeat'
-If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
-    Write-Host("Service $serviceName already exists on machine! Exiting...")
-    Exit
+$filebeatServiceName = 'filebeat'
+$filebeatFinalLocation = "C:\Program Files\Filebeat_tst"
+$filebeatExecutableName = "filebeat.exe"
+$filebeatConfigName = "filebeat.yml"
+
+
+
+If (!(Get-Service $filebeatServiceName -ErrorAction SilentlyContinue)) {
+    Write-Host("*****Installing filebeat*****")
+
+    $PWD = (Get-Item -Path ".\").FullName
+    $fullSrcPath = Join-Path $PWD "filebeat-6.5.4-windows-x86_64.zip"
+    $tmpDest = Join-Path $PWD "tmpDir"
+
+    Write-Host("*****Extracting $fullSrcPath to $tmpDest*****")
+    Expand-Archive $fullSrcPath $tmpDest
+    Write-Host("*****Finished extracting*****")
+
+    $filebeatContentDir = Get-ChildItem -Recurse -Filter $filebeatExecutableName | Select-Object -ExpandProperty DirectoryName -Unique
+    Write-Host("*****Found $filebeatExecutableName in $filebeatContentDir - copying to $filebeatFinalLocation*****")
+    robocopy $filebeatContentDir $filebeatFinalLocation /MIR /W:5 /R:2 
+    Write-Host("*****Finished copying, deleting $tmpDest*****")
+    Remove-Item –path "$tmpDest" –recurse 
+
+    # & $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
+
+    Write-Host("*****Finished installing filebeat*****")
+}
+
+If ((Get-Service $filebeatServiceName).Status -eq 'Running') {
+    Stop-Service $filebeatServiceName
+    Write-Host "Stopping $filebeatServiceName"
 }
 
 
-$PWD = (Get-Item -Path ".\").FullName
-$fullSrcPath = Join-Path $PWD "filebeat-6.5.4-windows-x86_64.zip"
-$dest = Join-Path $PWD "tmpDir"
+$srcFileName = "filebeat_template.yml"
+$destFileName = Join-Path $filebeatFinalLocation $filebeatConfigName
 
-Write-Host("*****Extracting $fullSrcPath to $dest*****")
-Expand-Archive $fullSrcPath $dest
-Write-Host("*****Finished extracting*****")
+$privateKey = 'PRIVATE_KEY: "myPrivateKey"'
+$companyId = 'COMPANY_ID: "myCompanyId"'
+$appName = 'APP_NAME: "myAppName"'
+$subsystem = 'SUB_SYSTEM: "mySubsystem1"'
 
-$filebeatExecutableName = "filebeat.exe"
-$filebeatContentDir = Get-ChildItem -Recurse -Filter $filebeatExecutableName | Select-Object -ExpandProperty DirectoryName -Unique
-# Join-Path $PWD $filebeatContentDir "*"
-$filebeatFinalLocation = "C:\tmp\tmp folder with spaces\existingDir\newDir"#"C:\Program Files\Filebeat"
-Write-Host("*****Found $filebeatExecutableName in $filebeatContentDir - copying to $filebeatFinalLocation*****")
-robocopy $filebeatContentDir $filebeatFinalLocation /MIR /W:5 /R:2 
-Write-Host("*****Finished copying, deleting $fullSrcPath*****")
-Write-Host($dest)
-Remove-Item –path "$dest" –recurse 
+Write-Host "Updating $destFileName"
 
+(Get-Content $srcFileName) `
+    -replace 'PRIVATE_KEY: .*', $privateKey `
+    -replace 'COMPANY_ID: .*', $companyId `
+    -replace 'APP_NAME: .*', $appName `
+    -replace 'SUB_SYSTEM: .*', $subsystem |
+    Out-File $destFileName
 
-
-
-# & $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
-
-
-
-# function DoesEerviceExist($serviceName) {
-#     If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
-
-#         If ((Get-Service $serviceName).Status -eq 'Running') {
-
-#             Stop-Service $serviceName
-#             Write-Host "Stopping $serviceName"
-
-#         }
-#         Else {
-
-#             Write-Host "$serviceName found, but it is not running."
-
-#         }
-
-#     }
-#     Else {
-
-#         Write-Host "$serviceName not found"
-
-#     }
-# }
+Write-Host "Starting service $filebeatServiceName"
+Start-Service $filebeatServiceName
+# TODO:
+# 1) Restart the service (ideally by executing "filebeat.exe -e" - if that doesn't work out , by Start-Service or something like that)
+# 2) Exit (as not to continue to other part)
