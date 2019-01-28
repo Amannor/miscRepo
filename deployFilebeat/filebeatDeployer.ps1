6891ec4b631d7fff6cceb394f99fb48322c1636e
Amannor <alon.mannor@gmail.com>
2019-01-28 20:55:03 +0200

    Add files via upload
    Updated script

--- deployFilebeat/filebeatDeployer.ps1
+++ deployFilebeat/filebeatDeployer.ps1
@@ -1,15 +1,18 @@
-#Usage example:
-#.\filebeatDeployer.ps1 -archiveFileFullPath C:\sandbox\deployFileBeat\script\filebeat-6.5.4-windows-x86_64.zip  -configTemplateFullPath C:\sandbox\deployFileBeat\script\filebeat_template.yml
+﻿#Usage example:
+#.\filebeatDeployer.ps1 -appName ExplorationEngine.Web  -subsystem TSS_QA
 param
 (
-    [string] $archiveFileFullPath = "a", #Join-Path $PWD "filebeat-6.5.4-windows-x86_64.zip"
-    [string] $configTemplateFullPath = "b" #Join-Path $PWD "filebeat_template.yml"
+    [string] $appName  = "ExplorationEngine.Web",
+    [string] $subsystem  = "TSS_QA"
 )
 
 $PWD = (Get-Item -Path ".\").FullName
 
+$archiveFileFullPath = Join-Path $PWD "filebeat-6.5.4-windows-x86_64.zip"
+$configTemplateFullPath = Join-Path $PWD "filebeat_template.yml"
+
 $filebeatServiceName = 'filebeat'
-$filebeatFinalLocation = "C:\Program Files\Filebeat_tst"
+$filebeatFinalLocation = "C:\Program Files\Filebeat"
 $filebeatExecutableName = "filebeat.exe"
 $filebeatConfigName = "filebeat.yml"
 
@@ -30,7 +33,8 @@ If (!(Get-Service $filebeatServiceName -ErrorAction SilentlyContinue)) {
     Write-Host("*****Finished copying, deleting $tmpDest*****")
     Remove-Item –path "$tmpDest" –recurse 
 
-    # & $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
+    $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
+    & $installScriptFullPath
 
     Write-Host("*****Finished installing filebeat*****")
 }
@@ -43,22 +47,21 @@ If ((Get-Service $filebeatServiceName).Status -eq 'Running') {
  
 $destFileName = Join-Path $filebeatFinalLocation $filebeatConfigName
 
-$privateKey = 'PRIVATE_KEY: "myPrivateKey"'
-$companyId = 'COMPANY_ID: "myCompanyId"'
-$appName = 'APP_NAME: "myAppName"'
-$subsystem = 'SUB_SYSTEM: "mySubsystem1"'
+# $privateKey = "PRIVATE_KEY: \`"$privateKey \`""
+# $companyId = "COMPANY_ID: \`"$companyId \`"" 
+# $appName = 'APP_NAME: "'+${appName}+'"'
+$appName = "APP_NAME: \`"$appName \`""
+# $subsystem = 'SUB_SYSTEM: "mySubsystem1"'
+$subsystem = "SUB_SYSTEM: \`"$subsystem\`""
 
 Write-Host "Updating $destFileName"
 
 (Get-Content $configTemplateFullPath) `
-    -replace 'PRIVATE_KEY: .*', $privateKey `
-    -replace 'COMPANY_ID: .*', $companyId `
+    # -replace 'PRIVATE_KEY: .*', $privateKey `
+    # -replace 'COMPANY_ID: .*', $companyId `
     -replace 'APP_NAME: .*', $appName `
     -replace 'SUB_SYSTEM: .*', $subsystem |
     Out-File $destFileName
 
 Write-Host "Starting service $filebeatServiceName"
 Start-Service $filebeatServiceName
-# TODO:
-# 1) Restart the service (ideally by executing "filebeat.exe -e" - if that doesn't work out , by Start-Service or something like that)
-# 2) Exit (as not to continue to other part)
