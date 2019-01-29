c97c975c2bc9f1e4ffc7a1e953e121b89ef8847a
Amannor <alon.mannor@gmail.com>
2019-01-29 19:43:51 +0200

    Updated deployer file

--- deployFilebeat/filebeatDeployer.ps1
+++ deployFilebeat/filebeatDeployer.ps1
@@ -1,5 +1,5 @@
 #Usage example:
-#PS > .\filebeatDeployer.ps1 -appName ExplorationEngine.Web  -subsystem TSS_QA
+#.\filebeatDeployer.ps1 -appName ExplorationEngine.Web -subsystem TSS_QA -wd C:\LoggingArtifacts
 param
 (
     [string] $appName  = "ExplorationEngine.Web",
@@ -8,45 +8,47 @@ param
 )
 
 
-
 $archiveFileFullPath = Join-Path $wd "filebeat-6.5.4-windows-x86_64.zip"
 $configTemplateFullPath = Join-Path $wd "filebeat_template.yml"
 
-$filebeatServiceName = 'filebeat'
-$filebeatFinalLocation = "C:\Program Files\Filebeat"
-$filebeatExecutableName = "filebeat.exe"
-$filebeatConfigName = "filebeat.yml"
+$serviceName = 'filebeat'
+$finalLocation = "C:\Program Files\Filebeat"
+$exeName = "filebeat.exe"
+$configName = "filebeat.yml"
 
 
 
-If (!(Get-Service $filebeatServiceName -ErrorAction SilentlyContinue)) {
-    Write-Host("*****Installing filebeat*****")
+If (!(Get-Service $serviceName -ErrorAction SilentlyContinue)) {
+    Write-Host("*****Installing $serviceName*****") #Taken from: https://www.elastic.co/guide/en/beats/filebeat/6.5/filebeat-installation.html (see "win")
 
     $tmpDest = Join-Path $wd "tmpDir"
 
     Write-Host("*****Extracting $archiveFileFullPath to $tmpDest*****")
-    Expand-Archive $archiveFileFullPath $tmpDest
+    Expand-Archive $archiveFileFullPath $tmpDest -Force
     Write-Host("*****Finished extracting*****")
 
-    $filebeatContentDir = Get-ChildItem -Recurse -Filter $filebeatExecutableName | Select-Object -ExpandProperty DirectoryName -Unique
-    Write-Host("*****Found $filebeatExecutableName in $filebeatContentDir - copying to $filebeatFinalLocation*****")
-    robocopy $filebeatContentDir $filebeatFinalLocation /MIR /W:5 /R:2 
+    $filebeatContentDir = Get-ChildItem -Path $tmpDest -Recurse -Filter $exeName | Select-Object -ExpandProperty DirectoryName -Unique
+    Write-Host("*****Found $exeName in $filebeatContentDir - copying to $finalLocation*****")
+    robocopy $filebeatContentDir $finalLocation /MIR /W:5 /R:2 
     Write-Host("*****Finished copying, deleting $tmpDest*****")
     Remove-Item –path "$tmpDest" –recurse 
 
-    $installScriptFullPath = Join-Path $filebeatFinalLocation "install-service-filebeat.ps1"
+    $installScriptFullPath = Join-Path $finalLocation "install-service-filebeat.ps1"
     & $installScriptFullPath
 
     Write-Host("*****Finished installing filebeat*****")
 }
 
-If ((Get-Service $filebeatServiceName).Status -eq 'Running') {
-    Stop-Service $filebeatServiceName
-    Write-Host "Stopping $filebeatServiceName"
+If ((Get-Service $serviceName).Status -eq 'Running') {
+    Stop-Service $serviceName
+    Write-Host "Stopping $serviceName"
 }
 
+$certificationFile = Join-Path $wd "ca.crt"
+Copy-Item $certificationFile $finalLocation #Copy-Item and not robocopy because $finalLocation doesn't contain a trailing backslash
+
  
-$destFileName = Join-Path $filebeatFinalLocation $filebeatConfigName
+$destFileName = Join-Path $finalLocation $configName
 
 $appName = "APP_NAME: `"$appName`""
 $subsystem = "SUB_SYSTEM: `"$subsystem`""
@@ -58,5 +60,5 @@ Write-Host "Updating $destFileName"
     -replace 'SUB_SYSTEM: .*', $subsystem |
     Out-File $destFileName
 
-Write-Host "Starting service $filebeatServiceName"
-Start-Service $filebeatServiceName
+Write-Host "Starting service $serviceName"
+Start-Service $serviceName
